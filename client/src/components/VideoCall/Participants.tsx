e4e951dc5fdb18e938f85d1ead2869f957124bf4
Wojtek <wojciechkrol97@gmail.com>
2020-11-15 11:51:58 +0100

    CAL-7 videocall

---
+++ client/src/components/VideoCall/Participants.tsx
@@ -0,0 +1,37 @@
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import Participant from './Participant';
+import Preview from './Preview';
+
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    display: 'grid',
+    height: '60vh',
+    gridGap: theme.spacing(3),
+    gridTemplateColumns: 'repeat(3, 50%)',
+    overflow: 'hidden',
+    [theme.breakpoints.down('xs')]: {
+      height: '70vh',
+      display: 'flex',
+      flexDirection: 'column'
+    }
+  }
+}));
+
+const Participants = () => {
+  const cx = useStyles();
+
+  return (
+    <div className={cx.grid}>
+      <div>
+        {' '}
+        <Preview />
+      </div>
+      <div>
+        <Participant />
+      </div>
+    </div>
+  );
+};
+
+export default Participants;
