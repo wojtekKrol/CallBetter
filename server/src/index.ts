9090ffe6186025e4a60348a9cb73b70999b378c9
Wojtek <wojciechkrol97@gmail.com>
2020-11-20 20:42:31 +0100

    CAL-18 working

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,9 @@ void mongoose.connect(DATABASE.MONGO_URI, {
   useUnifiedTopology: true,
 });
 mongoose.connection.on('error', console.error);
-mongoose.connection.once('open', () => console.log(chalk.cyan('MongoDB connected')));
+mongoose.connection.once('open', () =>
+  console.log(chalk.cyan('MongoDB connected')),
+);
 
 const app = express();
 
