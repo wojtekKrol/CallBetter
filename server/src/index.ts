37b481f8b25e5b2438f062a1c046d9e7401502f5
Wojtek <wojciechkrol97@gmail.com>
2020-11-23 12:25:11 +0100

    update dockerfile & compose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import chalk from 'chalk';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
@@ -15,7 +16,9 @@ void mongoose.connect(DATABASE.MONGO_URI, {
   useUnifiedTopology: true,
 });
 mongoose.connection.on('error', console.error);
-mongoose.connection.once('open', () => console.log('MongoDB connected'));
+mongoose.connection.once('open', () =>
+  console.log(chalk.yellow.bold('MongoDB connected')),
+);
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -28,7 +31,6 @@ app.use(express.json());
 app.get('/elo', (req, res) => {
   res.send('Hello World');
 });
-
 app.listen(<number>PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(chalk.blue.bold(`Server is running on http://localhost:${PORT}`));
 });
