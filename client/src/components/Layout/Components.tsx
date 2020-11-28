d48382d11c86ef9876e7bc2025165b17242a6b16
Wojtek <wojciechkrol97@gmail.com>
2020-11-28 14:01:32 +0100

    CAL-20 mobile landing page

--- client/src/components/Layout/Components.tsx
+++ client/src/components/Layout/Components.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import img from '../../assets/img.jpg';
 
 interface ContainerProps {
@@ -9,9 +10,23 @@ export const Container = styled.div<ContainerProps>`
   display: flex;
   min-height: 100vh;
   flex-direction: column;
-  background: ${({ welcomeScreen }) => (welcomeScreen ? 'url(' + img + ')' : 'white')};
+  background: ${({ welcomeScreen }) =>
+    welcomeScreen ? 'url(' + img + ')' : 'white'};
   background-size: cover;
   word-wrap: break-word;
+  @media (max-width: 400px) {
+    background: #0575e6; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to right,
+      #021b79,
+      #0575e6
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      270deg,
+      #021b79,
+      #0575e6
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
 `;
 
 export const Overlay = styled.div<ContainerProps>`
@@ -20,6 +35,9 @@ export const Overlay = styled.div<ContainerProps>`
     welcomeScreen
       ? 'linear-gradient(\n    227.23deg,\n    rgba(158, 0, 255, 0.54) 4.03%,\n    rgba(112, 0, 255, 0.54) 91.25%\n  );'
       : 'white'};
+  @media (max-width: 400px) {
+    background: transparent;
+  }
 `;
 
 export const ChildrenContainer = styled.div`
