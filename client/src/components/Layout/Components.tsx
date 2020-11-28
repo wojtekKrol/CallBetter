a4e5513ce4a220b6af64cb8e51247fa78afe1563
Wojtek <wojciechkrol97@gmail.com>
2020-11-28 14:16:01 +0100

    CAL-20 fixes

--- client/src/components/Layout/Components.tsx
+++ client/src/components/Layout/Components.tsx
@@ -15,17 +15,8 @@ export const Container = styled.div<ContainerProps>`
   background-size: cover;
   word-wrap: break-word;
   @media (max-width: 400px) {
-    background: #0575e6; /* fallback for old browsers */
-    background: -webkit-linear-gradient(
-      to right,
-      #021b79,
-      #0575e6
-    ); /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(
-      270deg,
-      #021b79,
-      #0575e6
-    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: ${({ welcomeScreen }) =>
+      welcomeScreen ? 'linear-gradient(270deg,#021b79,#0575e6)' : 'white'};
   }
 `;
 
