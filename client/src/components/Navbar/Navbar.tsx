3c43a6cebbd963598ab8d22dc0cdccb168e580e4
Wojtek <wojciechkrol97@gmail.com>
2020-11-30 12:12:19 +0100

    CAL-22 dialog logout

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import {
@@ -9,6 +9,7 @@ import {
 } from '../../constants/ConfigLayout';
 import RouteTypes from '../../constants/routes';
 import UserContext from '../../lib/UserContext';
+import DialogAlert from '../DialogAlert';
 import Logo from '../SVG/Logo';
 import { Nav, AuthButton } from './Components';
 
@@ -21,34 +22,53 @@ const Navbar = ({ welcomeScreen, authForm }: NavbarProps) => {
   const history = useHistory();
   const user = useContext(UserContext);
 
+  const title = 'Are you sure you want to sign out?';
+  const closeText = 'Back';
+  const confirmText = 'Sign out';
+  const [open, setOpen] = useState(false);
+
+  const confirmedCallLeave = () => {
+    localStorage.setItem('auth-token', '');
+    user.setUser({ logged: false, userDate: undefined, token: '' });
+    history.push('/');
+  };
+
   const navigation: LayoutItem[] = user?.user?.logged
     ? mainNavConfigUser
     : mainNavConfigAnonymous;
 
   const handleClick = (route: string) => {
     if (route === RouteTypes.INDEX) {
-      localStorage.setItem('auth-token', '');
-      user.setUser({ logged: false, userDate: undefined, token: '' });
-      history.push(route);
+      setOpen(true);
     } else {
       history.push(route);
     }
   };
 
   return (
-    <Nav welcomeScreen={welcomeScreen}>
-      <Logo />
-      {!authForm &&
-        navigation.map((subpage: LayoutItem) => (
-          <AuthButton
-            key={subpage.route}
-            welcomeScreen={subpage?.ws}
-            logOut={subpage.showFor === ForSessionState.AUTH}
-            onClick={() => handleClick(subpage.route)}>
-            {subpage.label}
-          </AuthButton>
-        ))}
-    </Nav>
+    <>
+      <DialogAlert
+        title={title}
+        confirmText={confirmText}
+        closeText={closeText}
+        state={open}
+        setStateBack={setOpen}
+        confirmFunction={confirmedCallLeave}
+      />
+      <Nav welcomeScreen={welcomeScreen}>
+        <Logo />
+        {!authForm &&
+          navigation.map((subpage: LayoutItem) => (
+            <AuthButton
+              key={subpage.route}
+              welcomeScreen={subpage?.ws}
+              logOut={subpage.showFor === ForSessionState.AUTH}
+              onClick={() => handleClick(subpage.route)}>
+              {subpage.label}
+            </AuthButton>
+          ))}
+      </Nav>
+    </>
   );
 };
 
