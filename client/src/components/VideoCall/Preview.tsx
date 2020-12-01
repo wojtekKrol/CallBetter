a051daf7cb784c1fa2b74e32b306172ec24f9662
Wojtek <wojciechkrol97@gmail.com>
2020-12-02 00:35:06 +0100

    CAL-15 call

--- client/src/components/VideoCall/Preview.tsx
+++ client/src/components/VideoCall/Preview.tsx
@@ -1,25 +1,7 @@
-import { makeStyles, Theme } from '@material-ui/core/styles';
 import React from 'react';
 
-const useStyles = makeStyles<Theme>((theme) => ({
-  video: {
-    maxWidth: '100%',
-    minWidth: '100%',
-    maxHeight: '100%',
-    minHeight: '100%',
-    backgroundColor: '#000',
-    objectFit: 'contain'
-  }
-}));
-
 const Preview = () => {
-  const cx = useStyles();
-
-  return (
-    <div>
-      <video className={cx.video} />
-    </div>
-  );
+  return <div />;
 };
 
 export default Preview;
--- client/src/lib/VideoContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const VideoContext = createContext<any>(null);
-
-export default VideoContext;
