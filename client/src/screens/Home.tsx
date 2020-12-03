a614cac25f8b78591f883e20d68e4fc57b808992
Wojtek <wojciechkrol97@gmail.com>
2020-12-03 01:34:29 +0100

    call history

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 import { useSnackbar } from 'notistack';
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -10,6 +10,10 @@ import './styles/home.css';
 import InfoIcon from '../components/SVG/InfoIcon';
 import { SERVER_URL } from '../constants/server';
 import UserContext from '../lib/UserContext';
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green } from '@material-ui/core/colors';
 
 const Home = () => {
   return (
@@ -88,33 +92,128 @@ const CameraPreview = () => {
   );
 };
 
-type CallHistoryRecord = {
-  name: string;
-  startTime: string;
-  endTime: string;
+type CallProps = {
+  readonly id: string;
+  endDate?: Date;
+  hostId: string;
+  guestId?: string;
+  role?: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+  interlocutorId?: string;
+  interlocutorName?: string;
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    wrapper: {
+      margin: theme.spacing(1),
+      position: 'relative',
+    },
+    buttonProgress: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginTop: -10,
+      marginLeft: -20,
+    },
+  }),
+);
+
 const HitoryPreview = () => {
-  const historyRecords = [
-    { name: 'Mati', startTime: '19:38', endTime: '19:44' },
-  ];
+  const cx = useStyles();
+  const [limit, setLimit] = useState<number>(10);
+  const [fetchedCalls, setFechedCalls] = useState<any>([]);
+  const [calls, setCalls] = useState<any>([]);
+  const user = useContext(UserContext);
+  const token = localStorage.getItem('auth-token');
+  const userId = user.user.userData.id;
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const getHistoryCalls = async () => {
+      try {
+        setLoading(true);
+        const historyCalls = await Axios.get(`${SERVER_URL}call/myCalls`, {
+          headers: {
+            'x-auth-token': token,
+            'Content-Type': 'application/json',
+          },
+          params: {
+            userId,
+            limit,
+          },
+        });
+        setFechedCalls(historyCalls.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // eslint-disable-next-line no-void
+    void getHistoryCalls();
+  }, [limit, token, userId]);
+
+  useEffect(() => {
+    const readableCalls = async (calls: CallProps[], userId: string) => {
+      try {
+        let results = calls.filter((call: CallProps) => call.guestId);
+
+        results = results.map((call: CallProps) => {
+          const interlocutorId =
+            call.hostId === userId ? call.guestId : call.hostId;
+          return { ...call, interlocutorId };
+        });
+
+        const filteredCalls = [];
+        for await (const result of results) {
+          const fetchedUser = await Axios.get(
+            `${SERVER_URL}users/getUserData`,
+            {
+              headers: {
+                'x-auth-token': token,
+                'Content-Type': 'application/json',
+              },
+              params: {
+                id: result.interlocutorId,
+              },
+            },
+          );
+          filteredCalls.push({
+            ...result,
+            interlocutorName: fetchedUser.data.name,
+          });
+        }
+        setCalls(filteredCalls);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    // eslint-disable-next-line no-void
+    void readableCalls(fetchedCalls, userId);
+  }, [fetchedCalls, token, userId]);
 
   return (
     <div className="historyWrapper">
       <div className="historyTitle">Connection history</div>
       <div className="historyRecords">
-        {historyRecords.map((record: CallHistoryRecord) => (
-          <div className="historyRecord">
-            <div className="recordName">{record.name}</div>
+        {calls.map((record: CallProps) => (
+          <div className="historyRecord" key={record.createdAt.toString()}>
+            <div className="recordName">{record.interlocutorName}</div>
             <div>
-              {record.startTime} <Link to="/home" component={InfoIcon} />
+              {new Date(record.createdAt).toLocaleDateString()}
+              <Link to="/home" component={InfoIcon} />
             </div>
           </div>
         ))}
       </div>
-      <button className="historyButton" onClick={() => console.log('ts')}>
-        Show more
-      </button>
+      <div className={cx.wrapper}>
+        <button className="historyButton" onClick={() => setLimit(limit + 10)}>
+          Show more
+        </button>
+        {loading && <CircularProgress className={cx.buttonProgress} />}
+      </div>
     </div>
   );
 };
